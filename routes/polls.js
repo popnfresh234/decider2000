8af2246eaf72769cd081517788ef93bece4b0501
Alex Holliday <ajhollid@gmail.com>
2018-04-12 05:41:32 +0000

    Adds semantic UI

---
+++ routes/polls.js
@@ -0,0 +1,152 @@
+const express = require('express');
+const router  = express.Router();
+
+
+require('dotenv').config();
+//const twilio = require('twilio');
+
+module.exports = (knex) => {
+
+  //var client = new twilio(process.env.TWILIO_SID, process.env.TWILIO_AUTH_TOKEN);
+
+  function insertOptions(optionArray, id) {
+    optionArray.forEach((option) => {
+      let title = option.title;
+      let description = option.description;
+      knex('option')
+      .insert({title, description, poll_id: id[0]})
+      .then((err) => {
+        if (err) {
+          console.log(err);
+        }
+      }).catch((err) => {
+        console.log(err);
+      });
+    });
+  }
+
+  function insertPhoneNumbers(phoneNumberArray, id){
+    if (phoneNumberArray && phoneNumberArray.length > 0) {
+      phoneNumberArray.forEach((number) => {
+        knex('phone')
+        .insert({number, poll_id: id[0]})
+        .then((err) => {
+          console.log(err);
+        }).catch((err) => {
+          console.log(err);
+        });
+      });
+    }
+  }
+
+
+  //*********************************************
+  //*** GET polls/:id/ ***
+  //*********************************************
+
+
+  router.get("/:id", (req, res)=> {
+    let id = req.params.id;
+    knex('poll')
+    .innerJoin('option', 'poll.id', '=', 'option.poll_id')
+    .where('poll.id', id)
+    .then((results) => {
+      res.render('poll', {results});
+    }).catch((err) => {
+      console.log(err);
+    });
+  });
+
+
+  //*********************************************
+  //*** GET polls/:id/links ***
+  //*********************************************
+
+  router.get("/:id/links", (req, res) => {
+    let id = req.params.id;
+    knex('phone')
+    .where('poll_id', id)
+    .then((results) => {
+
+      }).catch((err) => {
+        console.log(err);
+      });
+    res.render('links');
+  });
+
+  //*********************************************
+  //*** GET /polls/:id/result ***
+  //*********************************************
+
+
+  router.get("/:id/result", (req, res)=> {
+    let id = req.params.id;
+    knex('poll')
+    .join('option', 'poll.id', '=', 'option.poll_id')
+    .where('poll.id', id)
+    .orderBy('rank', 'desc')
+    .then((results) => {
+      res.render('results', {results});
+    }).catch((err) => {
+      console.log(err);
+    });
+  });
+
+
+  //*********************************************
+  //*** POST /polls/ ***
+  //*********************************************
+
+
+  router.post("/", (req, res) => {
+    let poll = req.body;
+    //Get poll data form poll object
+    let title = poll.ptitle;
+    let email = poll.email;
+    let optionArray = poll.options;
+    let phoneNumberArray = poll.phoneNumbers;
+
+    //Insert poll
+    knex('poll')
+    .returning('id')
+    .insert({ptitle: title, email})
+    .then((id) =>  {
+      insertOptions(optionArray, id);
+      insertPhoneNumbers(phoneNumberArray, id);
+      res.send({redirect: '/polls/' + id +'/links'});
+    }).catch((err) => {
+      console.log(err);
+    });
+  });
+
+
+  //*********************************************
+  //*** PUT/ polls/:id ***
+  //*********************************************
+
+  router.put("/:id", (req, res) => {
+    let id = req.params.id;
+    console.log("THIS IS THE ID:", id);
+    knex('poll')
+    .select('email')
+    .where('id', id)
+    .then((result) => {
+      let email = result[0].email;
+      //do whatever with email
+    }).catch((err) => {
+      console.log(err);
+    });
+
+    let optionsArray = req.body.data;
+    optionsArray.forEach((option) => {
+      knex('option')
+      .increment('rank', option.rank)
+      .where('id', option.option_id)
+      .then((err) => {
+        console.log(err);
+      });
+    });
+    res.send({redirect: '/'});
+  });
+  return router;
+};
--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-"use strict";
+//"use strict";
 
 require('dotenv').config();
 
@@ -15,7 +15,8 @@ const morgan      = require('morgan');
 const knexLogger  = require('knex-logger');
 
 // Seperated Routes for each Resource
-const usersRoutes = require("./routes/users");
+const indexRoute = require("./routes/index");
+const pollRoutes = require("./routes/polls");
 
 // Load the logger first so all (static) HTTP requests are logged to STDOUT
 // 'dev' = Concise output colored by response status for development use.
@@ -27,18 +28,22 @@ app.use(knexLogger(knex));
 
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use("/styles", sass({
-  src: __dirname + "/styles",
-  dest: __dirname + "/public/styles",
-  debug: true,
-  outputStyle: 'expanded'
-}));
+
 app.use(express.static("public"));
 
 // Mount all resource routes
-app.use("/api/users", usersRoutes(knex));
+app.use("/", indexRoute());
 
-// Home page
+app.use("/polls", pollRoutes(knex));
+app.use("/styles", sass({
+  src: __dirname + "/styles",
+  dest: __dirname + "/public/styles",
+  force: true,
+  debug: true,
+  indentedSyntax: false,
+  outputStyle: 'expanded'
+}));
+
 app.get("/", (req, res) => {
   res.render("index");
 });

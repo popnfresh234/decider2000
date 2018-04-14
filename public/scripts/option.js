a9601364aef93b162e0cffd72a6be033c9663ab4
Alex Holliday <ajhollid@gmail.com>
2018-04-14 05:03:04 +0000

    Fixes CSS style for list, adds input put validaiton for options page

--- public/scripts/option.js
+++ public/scripts/option.js
@@ -1,5 +1,8 @@
 $(() => {
 
+  let inputTitle = $('#input-option')
+  let inputDescription = $('#input-description')
+
   function postOptions(optionArray){
     let id = $('h1').data('id');
 
@@ -20,20 +23,32 @@ $(() => {
   var hidden = true;
 
   $('#button-add-option').on('click', function(event){
-    var optionName = $('#input-option').find('input').val();
-    var description = $('#input-description').find('input').val();
+    var optionName = inputTitle.find('input').val();
+    var description = inputDescription.find('input').val();
 
-    //Build and appendlist item
-    var item = $('<div class="item">');
-    var leftFloatedContent = $('<div class="left floated content">')
-    leftFloatedContent.append('<div class="header option-name">' + optionName + '</div>')
-    leftFloatedContent.append('<span class="option-description">' + description + '</span>');
-    item.append(leftFloatedContent);
-    var rightFloatedContent = $('<div class="right floated content">');
-    rightFloatedContent.append('<div class="ui button basic icon"><i class="times icon"></i></div>');
-    item.append(rightFloatedContent);
-    $('#list-options').append(item);
-  });
+    //Validate input for option name, desc not required
+    if(optionName === ''){
+      inputTitle.addClass('error');
+    } else {
+      inputTitle.removeClass('error');
+
+      //Build and appendlist item
+      var item = $('<div class="item">');
+      var leftFloatedContent = $('<div class="left floated content">')
+      leftFloatedContent.append('<div class="header option-name">' + optionName + '</div>')
+      leftFloatedContent.append('<span class="option-description">' + description + '</span>');
+      item.append(leftFloatedContent);
+      var rightFloatedContent = $('<div class="right floated content">');
+      rightFloatedContent.append('<div class="ui button basic icon"><i class="times icon"></i></div>');
+      item.append(rightFloatedContent);
+      $('#list-options').append(item);
+
+      //Clear out fields
+      inputTitle.find('input').val('');
+      inputDescription.find('input').val('');
+   }
+
+});
 
   $('#list-options').on('click', '.button', function(event) {
     $(this).closest('.item').remove();
